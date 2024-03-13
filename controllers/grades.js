3191da4f5270bb68313bb64d359aa5926d79840d
Ali Albanna <ali.al-banna@outlook.com>
2024-03-13 11:27:11 +0300

    populate

--- controllers/grades.js
+++ controllers/grades.js
@@ -4,7 +4,10 @@ const Student = require("../models/Student")
 const gradeStudent = async (req, res) => {
   const studentId = req.params.studentId
   try {
-    let grades = await Grade.find({ studentId: studentId })
+    let grades = await Grade.find({ studentId: studentId }).populate([
+      "studentId",
+      "courseId",
+    ])
     res.json(grades)
   } catch (error) {
     res.json({ error: error.message })
